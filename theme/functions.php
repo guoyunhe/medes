d9c3009144d0d4c5a37a131d8ec795ebeb6c8f1c
Guo Yunhe <guoyunhebrave@gmail.com>
2015-02-10 11:48:14 +0200

    Basic theme function settings.

--- theme/functions.php
+++ theme/functions.php
@@ -17,3 +17,52 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+function schoolunion_setup() {
+    // Post thumbnail
+    add_theme_support('post-thumbnails');
+    set_post_thumbnail_size(700, 400, true);
+    add_image_size('audio-thumb', 600, 300);
+    // HTML5 search form
+    add_theme_support('html5', array('search-form'));
+    // Editor style
+    add_editor_style(get_stylesheet_directory_uri() . '/editor.css');
+}
+
+add_action('after_setup_theme', 'schoolunion_setup');
+
+/**
+ * JavaScript & CSS
+ */
+function schoolunion_scripts() {
+    wp_enqueue_style('theme-style', get_stylesheet_uri(), array(), '20150210.1');
+    wp_enqueue_style('font-awesome-style', get_stylesheet_directory_uri() . '/font-awesome/css/font-awesome.min.css', array(), '4.3.0');
+    wp_enqueue_script('theme-script', get_stylesheet_directory_uri() . '/script.js', array('jquery'));
+}
+
+add_action('wp_enqueue_scripts', 'schoolunion_scripts');
+
+
+/**
+ * Menu
+ */
+function schoolunion_menu_init() {
+  register_nav_menu('primary-menu', 'Primary Menu');
+}
+add_action( 'init', 'schoolunion_menu_init' );
+
+
+/**
+ * Title
+ */
+function schoolunion_title($title) {
+    if (is_feed()) {
+        return $title;
+    }
+
+    // Add the site name.
+    $title .= get_bloginfo('name', 'display');
+
+    return $title;
+}
+
+add_filter('wp_title', 'schoolunion_title', 10, 2);
