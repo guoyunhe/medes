def3ca8426b0290a1d81d52593c4fbb653f742a1
Guo Yunhe <guoyunhebrave@gmail.com>
2015-03-22 10:39:01 +0200

    Plugin post types and user properties.

---
+++ plugin/schoolunion.php
@@ -0,0 +1,510 @@
+<?php
+
+/**
+ * Copyright (C) 2015 Guo Yunhe <guoyunhebrave at gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Plugin Name: School Union
+ * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Description: A brief description of the plugin.
+ * Version: The plugin's version number. Example: 1.0.0
+ * Author: Name of the plugin author
+ * Author URI: http://URI_Of_The_Plugin_Author
+ * Text Domain: Optional. Plugin's text domain for localization. Example: mytextdomain
+ * Domain Path: Optional. Plugin's relative directory path to .mo files. Example: /locale/
+ * Network: Optional. Whether the plugin can only be activated network wide. Example: true
+ * License: A short license name. Example: GPL2
+ */
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+
+/**
+ * Register Form Extend
+ * 
+ * Added:
+ * 1. Real name
+ * 2. Location
+ * 3. Years and schools (MEDes path)
+ */
+
+//1. Add a new form element...
+add_action('register_form', 'myplugin_register_form');
+
+function myplugin_register_form() {
+
+    $first_name = (!empty($_POST['first_name']) ) ? trim($_POST['first_name']) : '';
+    $last_name = (!empty($_POST['last_name']) ) ? trim($_POST['last_name']) : '';
+    ?>
+    <p>
+        <label for="first_name">First Name<br/>
+            <input type="text" name="first_name" id="first_name" class="input" value="<?php echo esc_attr(wp_unslash($first_name)); ?>" size="25" />
+        </label>
+    </p>
+    <p>
+        <label for="last_name">Last Name<br/>
+            <input type="text" name="last_name" id="last_name" class="input" value="<?php echo esc_attr(wp_unslash($last_name)); ?>" size="25" />
+        </label>
+    </p>
+    <p>
+        <label for="country">Living Country<br/>
+            <input type="text" name="country" id="country" class="input" size="25" />
+        </label>
+    </p>
+    <p>
+        <label for="city">Living City<br/>
+            <input type="text" name="city" id="city" class="input" size="25" />
+        </label>
+    </p>
+    <table>
+        
+        <tr>
+            <td>
+                <label for="first-year">1st Year<br/>
+                    <input type="text" name="first-year" id="first-year" class="input" size="4" />
+                </label>
+            </td>
+            <td style="padding-left: 5px;">
+                <label for="first-school">1st School<br/>
+                    <select name="first-school" id="first-school" class="input">
+                        <option>School A</option>
+                        <option>School B</option>
+                        <option>School C</option>
+                        <option>School D</option>
+                        <option>School E</option>
+                        <option>School F</option>
+                    </select>
+                </label>
+            </td>
+        </tr>
+        
+        <tr>
+            <td>
+                <label for="second-year">2nd Year<br/>
+                    <input type="text" name="second-year" id="second-year" class="input" size="4" />
+                </label>
+            </td>
+            <td style="padding-left: 5px;">
+                <label for="second-school">2nd School<br/>
+                    <select name="second-school" id="second-school" class="input">
+                        <option>School A</option>
+                        <option>School B</option>
+                        <option>School C</option>
+                        <option>School D</option>
+                        <option>School E</option>
+                        <option>School F</option>
+                    </select>
+                </label>
+            </td>
+        </tr>
+        
+        <tr>
+            <td>
+                <label for="third-year">3rd Year<br/>
+                    <input type="text" name="third-year" id="third-year" class="input" size="4" />
+                </label>
+            </td>
+            <td style="padding-left: 5px;">
+                <label for="third-school">3rd School<br/>
+                    <select name="third-school" id="third-school" class="input">
+                        <option>School A</option>
+                        <option>School B</option>
+                        <option>School C</option>
+                        <option>School D</option>
+                        <option>School E</option>
+                        <option>School F</option>
+                    </select>
+                </label>
+            </td>
+        </tr>
+    </table>
+    <?php
+}
+
+//2. Add validation. In this case, we make sure first_name is required.
+add_filter('registration_errors', 'myplugin_registration_errors', 10, 3);
+
+function myplugin_registration_errors($errors, $sanitized_user_login, $user_email) {
+
+    if (empty($_POST['first_name']) || !empty($_POST['first_name']) && trim($_POST['first_name']) == '') {
+        $errors->add('first_name_error', __('<strong>ERROR</strong>: You must include a first name.', 'mydomain'));
+    }
+
+    return $errors;
+}
+
+//3. Finally, save our extra registration user meta.
+add_action('user_register', 'myplugin_user_register');
+
+function myplugin_user_register($user_id) {
+    
+    // Real Name
+    
+    if (!empty($_POST['first_name'])) {
+        update_user_meta($user_id, 'first_name', trim($_POST['first_name']));
+    }
+    
+    if (!empty($_POST['last_name'])) {
+        update_user_meta($user_id, 'last_name', trim($_POST['last_name']));
+    }
+    
+    // Location
+    
+    if (!empty($_POST['country'])) {
+        update_user_meta($user_id, 'country', trim($_POST['country']));
+    }
+    
+    if (!empty($_POST['city'])) {
+        update_user_meta($user_id, 'city', trim($_POST['city']));
+    }
+    
+    // MEDes Path
+    
+    // 1st
+    
+    if (!empty($_POST['first-year'])) {
+        update_user_meta($user_id, 'first-year', trim($_POST['first-year']));
+    }
+    
+    if (!empty($_POST['first-school'])) {
+        update_user_meta($user_id, 'first-school', trim($_POST['first-school']));
+    }
+    
+    // 2nd
+    
+    if (!empty($_POST['second-year'])) {
+        update_user_meta($user_id, 'second-year', trim($_POST['second-year']));
+    }
+    
+    if (!empty($_POST['second-school'])) {
+        update_user_meta($user_id, 'second-school', trim($_POST['second-school']));
+    }
+    
+    // 3rd
+    
+    if (!empty($_POST['third-year'])) {
+        update_user_meta($user_id, 'third-year', trim($_POST['third-year']));
+    }
+    
+    if (!empty($_POST['third-school'])) {
+        update_user_meta($user_id, 'third-school', trim($_POST['third-school']));
+    }
+}
+
+/**
+ * Edit User Screen Extra Fields
+ */
+
+// 1. Extend edit user page, add more fields
+
+add_action('show_user_profile', 'su_show_extra_profile_fields');
+add_action('edit_user_profile', 'su_show_extra_profile_fields');
+
+function su_show_extra_profile_fields($user) {
+    ?>
+    
+    <h3>Photo</h3>
+    
+    <table class="form-table">
+        <tr>
+            <th><label for="su-avatar">Avatar</label></th>
+
+            <td>
+                <?php if (get_the_author_meta('su_avatar', $user->ID)) : ?>
+                <img src="<?php echo esc_attr(get_the_author_meta('su_avatar', $user->ID)); ?>"/><br/>
+                <?php endif; ?>
+                Add new avatar:
+                <input type="file" name="su-avatar" id="su-avatar" />
+            </td>
+        </tr>
+        
+        <tr>
+            <th><label for="su-photo">Photo</label></th>
+
+            <td>
+                <?php if (get_the_author_meta('su_photo', $user->ID)) : ?>
+                <img src="<?php echo esc_attr(get_the_author_meta('su_photo', $user->ID)); ?>"/><br/>
+                <?php endif; ?>
+                Add new photo:
+                <input type="file" name="su-photo" id="su-photo" />
+            </td>
+        </tr>
+    </table>
+    
+    <h3>Location</h3>
+    
+    <table class="form-table">
+        <tr>
+            <th><label for="city">City</label></th>
+
+            <td>
+                <input type="text" name="city" id="city"
+                       value="<?php echo esc_attr(get_the_author_meta('city', $user->ID)); ?>"
+                       class="regular-text" placeholder="city you are living"/>
+            </td>
+        </tr>
+        
+        <tr>
+            <th><label for="country">Country</label></th>
+
+            <td>
+                <input type="text" name="country" id="country"
+                       value="<?php echo esc_attr(get_the_author_meta('country', $user->ID)); ?>"
+                       class="regular-text" placeholder="country you are living"/>
+            </td>
+        </tr>
+    </table>
+
+    <h3>MEDes Path</h3>
+
+    <table class="form-table">
+        
+        <?php // first year ?>
+
+        <tr>
+            <th><label for="first-year">1st Year</label></th>
+
+            <td>
+                <input type="text" name="first-year" id="first-year"
+                       value="<?php echo esc_attr(get_the_author_meta('first_year', $user->ID)); ?>"
+                       class="regular-text" placeholder="year"/>
+            </td>
+        </tr>
+        
+        <tr>
+            <th><label for="first-school">1st School</label></th>
+
+            <td>
+                <input type="text" name="first-school" id="first-school"
+                       value="<?php echo esc_attr(get_the_author_meta('first_school', $user->ID)); ?>"
+                       class="regular-text" placeholder="school name"/>
+            </td>
+        </tr>
+        
+        <?php // second year ?>
+
+        <tr>
+            <th><label for="second-year">2nd Year</label></th>
+
+            <td>
+                <input type="text" name="second-year" id="second-year"
+                       value="<?php echo esc_attr(get_the_author_meta('second_year', $user->ID)); ?>"
+                       class="regular-text" placeholder="year"/>
+            </td>
+        </tr>
+        
+        <tr>
+            <th><label for="second-school">2nd School</label></th>
+
+            <td>
+                <input type="text" name="second-school" id="second-school"
+                       value="<?php echo esc_attr(get_the_author_meta('second_school', $user->ID)); ?>"
+                       class="regular-text" placeholder="school name"/>
+            </td>
+        </tr>
+        
+        <?php // first year ?>
+
+        <tr>
+            <th><label for="third-year">3rd Year</label></th>
+
+            <td>
+                <input type="text" name="third-year" id="third-year"
+                       value="<?php echo esc_attr(get_the_author_meta('third_year', $user->ID)); ?>"
+                       class="regular-text" placeholder="year"/>
+            </td>
+        </tr>
+        
+        <tr>
+            <th><label for="third-school">3rd School</label></th>
+
+            <td>
+                <input type="text" name="third-school" id="third-school"
+                       value="<?php echo esc_attr(get_the_author_meta('third_school', $user->ID)); ?>"
+                       class="regular-text" placeholder="school name"/>
+            </td>
+        </tr>
+
+    </table>
+    
+    <script>
+        jQuery('form').attr('enctype', 'multipart/form-data');
+    </script>
+<?php
+}
+
+// 2. Save extra fields to database
+
+add_action('personal_options_update', 'su_save_extra_profile_fields');
+add_action('edit_user_profile_update', 'su_save_extra_profile_fields');
+
+function su_save_extra_profile_fields($user_id) {
+
+    if (!current_user_can('edit_user', $user_id))
+        return false;
+
+    /* Copy and paste this line for additional fields. Make sure to change 'twitter' to the field ID. */
+    update_user_meta($user_id, 'city', $_POST['city']);
+    update_user_meta($user_id, 'country', $_POST['country']);
+    update_user_meta($user_id, 'first_year', $_POST['first-year']);
+    update_user_meta($user_id, 'first_school', $_POST['first-school']);
+    update_user_meta($user_id, 'second_year', $_POST['second-year']);
+    update_user_meta($user_id, 'second_school', $_POST['second-school']);
+    update_user_meta($user_id, 'third_year', $_POST['third-year']);
+    update_user_meta($user_id, 'third_school', $_POST['third-school']);
+    
+    error_log( print_r( $_FILES, true ) );
+    
+    if (!function_exists('wp_handle_upload')) {
+        require_once( ABSPATH . 'wp-admin/includes/file.php' );
+    }
+    
+    if (isset($_FILES['su-avatar'])) {
+        $uploadedfile = $_FILES['su-avatar'];
+        $upload_overrides = array('test_form' => false);
+        $upload_result = wp_handle_upload($uploadedfile, $upload_overrides);
+        if (isset($upload_result['file'])) {
+            unlink(get_user_meta( $user_id, 'su_avatar_file', true ));
+            update_user_meta($user_id, 'su_avatar', $upload_result['url']);
+            update_user_meta($user_id, 'su_avatar_file', $upload_result['file']);
+        }
+    }
+    
+    if (isset($_FILES['su-photo'])) {
+        $uploadedfile = $_FILES['su-photo'];
+        $upload_overrides = array('test_form' => false);
+        $upload_result = wp_handle_upload($uploadedfile, $upload_overrides);
+        if (isset($upload_result['file'])) {
+            unlink(get_user_meta( $user_id, 'su_photo_file', true ));
+            update_user_meta($user_id, 'su_photo', $upload_result['url']);
+            update_user_meta($user_id, 'su_photo_file', $upload_result['file']);
+        }
+    }
+}
+
+/**
+ * Custom Post Types
+ * 
+ * School
+ * 
+ * Workshop Series (name, featured image/logo)
+ * |-- WorkShop (year, featured image)
+ *     |-- Project (name, authors, school, featured image, content)
+ * 
+ */
+
+add_action('init', 'su_create_post_type');
+
+function su_create_post_type() {
+    register_post_type(
+        'su_school',
+        array(
+            'labels' => array(
+                'name' => 'Schools',
+                'singular_name' => 'School',
+            ),
+            'supports' => array(
+                'title',
+                'editor',
+                'thumbnail',
+                'excerpt',
+                'revisions',
+                'custom-fields',
+                'comments'
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'show_in_menu' => true,
+            'show_in_nav_menus' => TRUE,
+            'menu_icon' => 'dashicons-welcome-learn-more'
+        )
+    );
+    
+    register_post_type(
+        'su_workshop_series',
+        array(
+            'labels' => array(
+                'name' => 'Workshop Series',
+                'singular_name' => 'Workshop Series'
+            ),
+            'supports' => array(
+                'title',
+                'editor',
+                'thumbnail',
+                'excerpt',
+                'revisions',
+                'custom-fields',
+                'comments'
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'show_in_menu' => true,
+            'menu-icon' => 'dashicons-tickets-alt',
+        )
+    );
+    
+    register_post_type(
+        'su_workshop',
+        array(
+            'labels' => array(
+                'name' => 'Workshops',
+                'singular_name' => 'Workshop',
+                'parent' => 'Parent Movie Review'
+            ),
+            'supports' => array(
+                'title',
+                'editor',
+                'thumbnail',
+                'excerpt',
+                'revisions',
+                'custom-fields',
+                'comments'
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'show_in_menu' => true,
+            'hierarchical' => true,
+        )
+    );
+    
+    register_post_type(
+        'su_project',
+        array(
+            'labels' => array(
+                'name' => 'Projects',
+                'singular_name' => 'Project'
+            ),
+            'supports' => array(
+                'title',
+                'editor',
+                'thumbnail',
+                'excerpt',
+                'revisions',
+                'custom-fields',
+                'comments'
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'show_in_menu' => true,
+            'menu-icon' => 'dashicons-tickets-alt',
+        )
+    );
+}
+
+/**
+ * 
+ */
